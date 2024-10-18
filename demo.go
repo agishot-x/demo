8a8392663fab32b5ca60f3a3e3e1f606de9f2fa0
AgishotX <agishotx@gmail.com>
2024-10-18 17:51:13 +0900

    Update demo.go

--- demo.go
+++ demo.go
@@ -1,11 +1,33 @@
 package demo
 
-type tableName struct {
+type Column struct {
+	name string
+	value any
+}
+
+type ColumnOp struct {
+	Column
+	op string
+}
+
+type builder struct {
 	tableName string
+	columns []Column
+}
+
+type whereBuilder struct {
+	where []ColumnOp
 }
 
 type InsertBuilder struct {
-	tableName
-	columns []string
-	values []any
+	builder
+}
+
+type UpdateBuilder struct {
+	builder
+	whereBuilder
+}
+
+type DeleteBuilder struct {
+	whereBuilder
 }
